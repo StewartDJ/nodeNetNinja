0807093f9891c2a3dfb6547971f74a05b557a60a
DJ Stewart <deshastewart1992@gmail.com>
2020-08-18 23:07:40 -0700

    fix

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const { rest } = require("lodash");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const Blog = require('./models/blog');
+const { render } = require("ejs");
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.set("view engine", "ejs");
 
 //middleware and static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'));
 
 // app.get('/add-blog', (req, res) => {
@@ -70,9 +72,47 @@ app.get("/about", (req, res) => {
 app.get('/blogs', (req, res) =>{
   Blog.find().sort({createdAt: -1})
   .then((result) => {
-    res.render('index', {title: 'All Blogs', blogs: result })
+    res.render('index', { blogs: result, title: 'All blogs' });
   })
   .catch((err) => {
+    console.log(err);
+  });
+})
+
+app.post('/blogs', (req, res) => {
+ console.log(req.body);
+ const blog = new Blog(req.body);
+
+ blog.save()
+ .then((result) => { 
+   res.redirect('/blogs');
+ })
+ .catch((err) => {
+console.log(err);
+ })
+})
+
+
+//cannot find the id when console.loggign for some reason something went wrong before 16 min video mark in video 10
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then(result => {
+      res.render('details', { blog: result, title: 'Blog Details' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id; 
+  Blog.findByIdAndDelete(id)
+  .then(result => {
+    res.json({ redirect: '/blogs' })
+  })
+  .catch(err => {
     console.log(err);
   })
 })
