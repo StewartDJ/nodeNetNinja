0c0792fbc66df967e474a524c5393cadc0f331a7
DJ Stewart <deshastewart1992@gmail.com>
2020-08-31 13:41:44 -0700

    test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 const dbURI = "mongodb+srv://netninja:test1234@cluster0.khrdn.mongodb.net/nodetuts?retryWrites=true&w=majority";
 
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-.then((result) => app.listen(3000))
+.then((result) => app.listen(4000))
 .catch((err) => console.log(err));
 
 app.set("view engine", "ejs");
